439755713b20e567ca7319613067c7ea57f9f591
gprabaha <prabaha.gangopadhyay@yale.edu>
2020-10-21 20:08:03 -0400

    reconcile with template

--- script/pcta_plot_template.m
+++ script/pcta_plot_template.m
@@ -12,4 +12,14 @@ pcats = {'day'};  % make a panel for each day
 pl = plotlabeled.make_common();
 axs = pl.bar( response_time, labels, xcats, gcats, pcats );
 
-%%  
+%% plot reaction time
+
+reaction_time = basic_behavioral_data.reaction_time;
+labels = basic_behavioral_data.labels';
+
+xcats = {'subject'};
+gcats = {'patch-type'}; % make a group for each 'patch-type'
+pcats = {'day'};  % make a panel for each day
+
+pl = plotlabeled.make_common();
+axs = pl.bar( reaction_time, labels, xcats, gcats, pcats );
